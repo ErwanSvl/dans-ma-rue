9e927c51514604a2749d96c06c364d05efeebfb8
Erwan SEVELLEC <erwan.sevellec@imt-atlantique.net>
2020-02-18 15:28:01 +0100

    implements import from csv to elasticsearch

--- import.js
+++ import.js
@@ -1,14 +1,34 @@
 const config = require('config');
 const csv = require('csv-parser');
 const fs = require('fs');
-const { Client } = require('@elastic/elasticsearch');
+const {
+    Client
+} = require('@elastic/elasticsearch');
 const indexName = config.get('elasticsearch.index_name');
 
-async function run () {
+async function run() {
     // Create Elasticsearch client
-    const client = new Client({ node: config.get('elasticsearch.uri') });
+    const client = new Client({
+        node: config.get('elasticsearch.uri')
+    });
 
     // TODO il y a peut être des choses à faire ici avant de commencer ... 
+
+    // Création de l'indice
+    client.indices.create({
+        index: 'incidents',
+        body: {
+            mappings : {
+                properties : {
+                    location : { "type" : "geo_point" }
+                }
+            }
+        }
+    }, (err, resp) => {
+        if (err) console.trace(err.message);
+    });
+
+    let incidents = []
 
     // Read CSV file
     fs.createReadStream('dataset/dans-ma-rue.csv')
@@ -16,13 +36,81 @@ async function run () {
             separator: ';'
         }))
         .on('data', (data) => {
-            // TODO ici on récupère les lignes du CSV ...
-            console.log(data);
+                incidents.push({
+                    "timestamp": data.DATEDECL,
+                    "object_id": data.OBJECTID,
+                    "annee_declaration": data["ANNEE DECLARATION"],
+                    "mois_declaration": data["MOIS DECLARATION"],
+                    "type": data.TYPE,
+                    "sous_type": data.SOUSTYPE,
+                    "code_postal": data.CODE_POSTAL,
+                    "ville": data.VILLE,
+                    "arrondissement": data.ARRONDISSEMENT,
+                    "prefixe": data.PREFIXE,
+                    "intervenant": data.INTERVENANT,
+                    "conseil_de_quartier": data["CONSEIL DE QUARTIER"],
+                    "location": data.geo_point_2d
+                })
+                /*client.bulk({
+                    index: indexName,
+                    body: incidents
+                }).then(response => {
+                    
+                })*/
+            if (incidents.length / 20000 >= 1) {
+                client.bulk(createBulkInsertQuery(incidents), (err, resp) => {
+                    if (err) console.trace(err.message);
+                    else console.log(`Inserted ${resp.body.items.length} incidents`);
+                    client.close();
+                    console.log('Terminated!');
+                });
+                incidents = [];
+            }
         })
         .on('end', () => {
-            // TODO il y a peut être des choses à faire à la fin aussi ?
-            console.log('Terminated!');
+            client.bulk(createBulkInsertQuery(incidents), (err, resp) => {
+                if (err) console.trace(err.message);
+                else console.log(`Inserted ${resp.body.items.length} incidents`);
+                client.close();
+                console.log('Terminated!');
+            });
         });
 }
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(incidents) {
+    const body = incidents.reduce((acc, incident) => {
+        //console.log(incident)
+      const { timestamp, 
+        annee_declaration, 
+        mois_declaration, 
+        type,
+        sous_type,
+        code_postal,
+        ville,
+        arrondissement,
+        prefixe,
+        intervenant,
+        conseil_de_quartier,
+        location
+    } = incident;
+      acc.push({ index: { _index: 'incidents', _type: '_doc', _id: incident.object_id } })
+      acc.push({ timestamp, 
+        annee_declaration, 
+        mois_declaration, 
+        type,
+        sous_type,
+        code_postal,
+        ville,
+        arrondissement,
+        prefixe,
+        intervenant,
+        conseil_de_quartier,
+        location })
+      return acc
+    }, []);
+  
+    return { body };
+  }
 
 run().catch(console.error);
